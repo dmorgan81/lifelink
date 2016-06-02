09a1247565f92153b8c890b3b008e9bf5e95a4f9
David Morgan <dmorgan81@gmail.com>
2016-06-02 09:30:47 -0400

    Status bar and action bar

--- src/lifelink.c
+++ src/lifelink.c
@@ -2,13 +2,27 @@
 #include "logging.h"
 
 static Window *s_window;
+static ActionBarLayer *s_action_bar_layer;
+static StatusBarLayer *s_status_bar_layer;
 
 static void window_load(Window *window) {
     log_func();
+    Layer *root_layer = window_get_root_layer(window);
+    GRect bounds = layer_get_bounds(root_layer);
+
+    s_action_bar_layer = action_bar_layer_create();
+    action_bar_layer_add_to_window(s_action_bar_layer, window);
+
+    GRect frame = GRect(0, 0, bounds.size.w - ACTION_BAR_WIDTH, STATUS_BAR_LAYER_HEIGHT);
+    s_status_bar_layer = status_bar_layer_create();
+    layer_set_frame(status_bar_layer_get_layer(s_status_bar_layer), frame);
+    layer_add_child(root_layer, status_bar_layer_get_layer(s_status_bar_layer));
 }
 
 static void window_unload(Window *window) {
     log_func();
+    status_bar_layer_destroy(s_status_bar_layer);
+    action_bar_layer_destroy(s_action_bar_layer);
 }
 
 static void init(void) {
